40aacb65d035676f5b22490952c884cf8562d4b3
Alex Smith <asmith9455@gmail.com>
2018-08-18 17:57:23 -0400

    Further progress on the network

--- file_contents_gen.py
+++ file_contents_gen.py
@@ -11,6 +11,56 @@
 import matplotlib.image as mpimg
 import numpy as np
 
+def get_batches_multi_dir(directories, batch_size):
+    
+    i = 0
+    batch_ctr = 0
+
+    sw_angles = []
+    speed = []
+    throttle = []
+    brake_input = []
+    images = []
+
+    
+
+    for directory in directories:
+
+        print('opening data in ', directory)
+
+        for l in open(directory + "/driving_log.csv", 'r'):
+
+            data = l.split(",")
+
+            if i == batch_size:
+                
+                i = 0
+
+                batch_ctr = batch_ctr + 1
+
+                print('----GEN_DBG: ')
+                print('------batch_ctr: ', batch_ctr)
+
+                yield batch_ctr, np.array(images), np.array(sw_angles), np.array(throttle), np.array(brake_input), np.array(speed)
+
+                sw_angles = []
+                speed = []
+                throttle = []
+                brake_input = []
+                images = []
+
+            sw_angles.append(float(data[3]))
+            throttle.append(float(data[4]))
+            brake_input.append(float(data[5]))
+            speed.append(float(data[6]))
+            images.append([mpimg.imread(data[0])])#, mpimg.imread(data[1]), mpimg.imread(data[2])])
+
+            # print('----GEN_DBG: ')
+            # print('------i: ', i)
+
+            i = i + 1
+
+    yield batch_ctr, np.array(images), np.array(sw_angles), np.array(throttle), np.array(brake_input), np.array(speed)
 
 def get_batches(directory, batch_size):
     
@@ -52,11 +102,11 @@ def get_batches(directory, batch_size):
         speed.append(float(data[6]))
         images.append([mpimg.imread(data[0])])#, mpimg.imread(data[1]), mpimg.imread(data[2])])
 
-        print('----GEN_DBG: ')
-        print('------i: ', i)
+        # print('----GEN_DBG: ')
+        # print('------i: ', i)
 
         i = i + 1
-    
+
     yield batch_ctr, np.array(images), np.array(sw_angles), np.array(throttle), np.array(brake_input), np.array(speed)
 
         
--- model.py
+++ model.py
@@ -1,79 +1,156 @@
 from data_exploration import explore_over_time, frame_count, generate_summary_plot
 
-from file_contents_gen import get_batches
+from file_contents_gen import get_batches_multi_dir
 
 import matplotlib.pyplot as plt
 
 from keras.models import Sequential
-from keras.layers.core import Activation, Flatten, Dense, Lambda
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.core import Activation, Flatten, Dense, Lambda, Dropout
+# from tf.keras.layers import InputLayer
+from keras.layers import Cropping2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+# from  import tf as ktf
+# import tensorflow as tf
+# import keras
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import keras
 
 
-fname = "../data/P3-sim-data"
+fname = "../data/P3-sim-data-udacity/data"
+# fname = "../data/P3-sim-data-5"
 
-print("starting linecount...")
-
-line_count = frame_count(fname)
-
-print("this data file contains ", line_count, " lines.")
-
-line_count = 10
+dirs = ["../data/P3-sim-data-udacity/data"]
 
 # //steering: -1 to 1
 # // throttle 0 to 1
 # // brake 0 1
 # // speed 0 30
 
-images, sw_angles, throttle, brake_input, speeds = explore_over_time(fname, line_count)
+images, sw_angles, throttle, brake_input, speeds = explore_over_time(fname, 300)
 
-# generate_summary_plot(images, sw_angles, throttle, brake_input, speeds)
+generate_summary_plot(images, sw_angles, throttle, brake_input, speeds)
 
 model = Sequential()
 
-image_shape = images[0,0,:,:].shape
+image_shape = (70, 160, 3)# images[0,0,:,:].shape
 
-model.add(Lambda(lambda x: (x / 255.0 - 0.5) * 2, input_shape=image_shape))
+# model.add(InputLayer(input_shape=(None, 160, 320, 3)))
 
-model.add(Convolution2D(40, 5, 5, border_mode="valid"))
+model.add(Lambda(lambda x: __import__('tensorflow').image.rgb_to_grayscale(x)))
 
-model.add(Activation("relu"))
+model.add(Cropping2D(cropping=( (60,0), (0,0) )))
 
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+model.add(Lambda(lambda x: __import__('keras').backend.tf.image.resize_images(x, (50,160))))
 
-model.add(Convolution2D(80, 5, 5, border_mode="valid"))
+model.add(Lambda(lambda x: (x / 255.0 - 0.5) * 2))
 
-model.add(Activation("relu"))
+model.add(Conv2D(filters=12, kernel_size=5, strides=(1,1), activation='relu'))
 
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+model.add(Conv2D(filters=24, kernel_size=5, strides=(2,2), activation='relu'))
 
+model.add(Conv2D(filters=36, kernel_size=5, strides=(2,2), activation='relu'))
+
+model.add(Conv2D(filters=48, kernel_size=3, strides=(1,1), activation='relu'))
+
+model.add(Conv2D(filters=64, kernel_size=3, strides=(1,1), activation='relu'))
 
 model.add(Flatten())
 
+model.add(Dense(400, activation='relu'))
 
-model.add(Dense(400))
+model.add(Dense(600, activation='relu'))
 
-model.add(Activation("relu"))
+model.add(Dense(300, activation='relu'))
 
-model.add(Dense(200))
+model.add(Dense(100, activation='relu'))
 
-model.add(Activation("relu"))
+model.add(Dropout(0.5))
 
 model.add(Dense(1)) #steering wheel angle is the output
 
 # features = images[:,0,:,:]
 # labels = sw_angles
 
-model.compile(loss='mse', optimizer='adam')
+opt = keras.optimizers.Adam(lr=0.0001)
+
+model.compile(loss='mse', optimizer=opt)
 
 print('training start')
 
-for batch_ctr, images, sw_angles, throttle, brake_input, speeds in get_batches(fname, 128):
+bins = np.arange(-10.0,10.0,0.1)
+counts = np.arange(-10.0,10.0,0.1) * 0.0
+
+count_gt_zero = 0
+count_lt_zero = 0
+count_eq_zero = 0
+
+for batch_ctr, images, sw_angles, throttle, brake_input, speeds in get_batches_multi_dir(dirs, 128):
+    
+    for sw_angle in sw_angles:
+
+        if sw_angle > 0.0 or sw_angle < 0.0:
+            count_lt_zero = count_lt_zero + 1
+            count_gt_zero = count_gt_zero + 1
+        else:
+            count_eq_zero = count_eq_zero + 2
+
+        for sw_angle in sw_angles:
+            histo_loc = np.argmax(bins >= sw_angle)
+            counts[histo_loc] = counts[histo_loc] + 1
+
+        for sw_angle in sw_angles:
+            histo_loc = np.argmax(bins >= -1.0 * sw_angle)
+            counts[histo_loc] = counts[histo_loc] + 1
+
+print('count_gt_zero: ', count_gt_zero)
+print('count_lt_zero: ', count_lt_zero)
+print('count_eq_zero: ', count_eq_zero)
+
+fig = plt.figure()
+
+ax=plt.subplot(111)
+
+plt.plot(bins, counts)
+
+ax.set_xticks(np.arange(-10,10,0.1), minor=True)
+ax.set_xticks(np.arange(-10,10,1.0), minor=False)
+# ax.set_yticks(np.arange(0, np.max(counts)), minor=True)
+
+plt.grid(which='major', axis='both')
+plt.grid(which='minor', axis='both')
+
+plt.show()
+
+for batch_ctr, images, sw_angles, throttle, brake_input, speeds in get_batches_multi_dir(dirs, 128):
 
     print('batch_ctr: ', batch_ctr)
 
     features = images[:,0,:,:]
     labels = sw_angles
 
-    model.fit(features, labels, validation_split=0.3, shuffle=True, epochs=1, batch_size=64)
+    features_aug_1 = np.flip(features, 2)
+    labels_aug_1 = sw_angles * -1.0
+       
+    # if (labels[0] > 0.01):
+
+        # plt.subplot(211)
+
+        # plt.title('steering angle: ' + str(labels[0]))
+
+        # plt.imshow(features[0,:,:,:])
+
+        # plt.subplot(212)
+
+        # plt.title('steering angle: ' + str(labels_aug_1[0]))
+
+        # plt.imshow(features_aug_1[0,:,:,:])
+
+        # plt.show()
+
+    model.fit(features, labels, validation_split=0.2, shuffle=True, epochs=4, batch_size=64)
+    # model.fit(features_aug_1, labels_aug_1, validation_split=0.2, shuffle=True, epochs=4, batch_size=64)
 
 model.save('model.h5')
